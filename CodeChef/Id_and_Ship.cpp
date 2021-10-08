b5255ef98c5402b482b9328d41528541717dc5ff
Devrajsinh Jhala <jhaladevrajsinh11@gmail.com>
2021-10-08 09:33:30 +0530

    Added these Problems
    Added two problems which have the same concept and it is accepted.
    Hope it helps and adds value.
    Thank You

---
+++ CodeChef/Id_and_Ship.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int t; cin >> t;
+
+    while(t--){
+        char type; cin >> type;
+
+        if(type == 'B' || type == 'b'){
+            cout << "BattleShip" << endl;
+        }
+
+        else if(type == 'C' || type == 'c'){
+            cout << "Cruiser" << endl;
+        }
+
+        else if(type == 'D' || type == 'd'){
+            cout << "Destroyer" << endl;
+        }
+
+        else if(type == 'F' || type == 'f'){
+            cout << "Frigate" << endl;
+        }
+    }
+
+    return 0;
+}
