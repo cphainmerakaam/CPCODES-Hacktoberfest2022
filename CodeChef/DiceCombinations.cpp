db2382bb4ae3a299a42c1fd6b1ae57deafea10dc
varshaig1999 <62078868+varshaig1999@users.noreply.github.com>
2022-10-13 17:08:58 +0530

    created dice combination in cpp

---
+++ CodeChef/DiceCombinations.cpp
@@ -0,0 +1,31 @@
+// The Dice problem is a problem in which we have to find out the nuumber of ways to generatea sum n by throwing a dice 1 or more than one types.
+
+
+# include <bits/stdc++.h>
+using namespace std;
+
+# define MOD 1000000007
+
+int main() {
+    // For Fast I/O.
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0);
+
+    
+    int n;
+    cin >> n;
+    int arr[n + 1];
+    arr[0] = 1;
+    
+    for (int i = 1; i <= n; i++) {
+        arr[i] = 0;
+        for (int j = 1; j <= 6 && i - j >= 0; j++) {
+            arr[i] += arr[i - j];
+            arr[i] %= MOD;
+        }
+    }
+    
+    cout << arr[n] << endl;
+ 
+    return 0;
+}
